133d5ae9e47509018bac6c63a35723d10d1484b2
Haganeno <Haganeno@users.noreply.github.com>
2023-03-03 16:30:44 +0000

    Notes on API endpoints found

---
+++ requests.py
@@ -0,0 +1,22 @@
+URL = "https://api.green-got.com"
+SIGNIN = "/v2/signin"
+CODE = "/v2/check-login-code"
+USER = "/user"
+TRANSACTIONS = "/v2/transactions" # filters limit and startDate and updatedAt
+SCHEDULED = "/scheduled-transfers"
+CONTACTS = "/contacts"
+CARD_IMAGE = "/card-image"
+IMPACT_INDIVIDUAL = "/impact/individual"
+IMPACT_SHARED = "/impact/shared"
+RIB = "/rib"
+DEBIT_MANDATES = "/direct-debit-mandates"
+BANK_STATEMENT = "/download/bank-statement" # filter month and year
+
+#accept-encoding: gzip
+#authorization: Bearer 1OcegOqWOh5xIPvU3eldvwnFXfc
+#connection: Keep-Alive
+#host: api.green-got.com
+#if-modified-since: Fri, 03 Mar 2023 15:48:57 GMT
+#user-agent: okhttp/4.10.0
+#x-app-version: 1.6.12
+#x-mobile-unique-id: 102f034e-8b58-4e04-a1ab-613feb65d9bd
